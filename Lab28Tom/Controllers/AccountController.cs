2ca3b8d96d99f6a9794f2cf172301e244d139de5
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-20 11:43:01 -0700

    added new properties to RegisterViewModel and ApplicationUser

---
+++ Lab28Tom/Controllers/AccountController.cs
@@ -0,0 +1,140 @@
+﻿using Lab28Tom.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Lab28Tom.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> usermanager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = usermanager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel rvm, string returnUrl = null)
+        {
+
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = rvm.Email, Email = rvm.Email };
+                var result = await _userManager.CreateAsync(user, rvm.Password);
+
+                if (result.Succeeded)
+                {
+                    //List<Claim> myClaims = new List<Claim>();
+
+                    //Claim claim1 = new Claim(ClaimTypes.Name, rvm.FirstName + " " + rvm.LastName, ClaimValueTypes.String, issuer);
+                    //myClaims.Add(claim1);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AdminRegister(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdminRegister(RegisterViewModel arvm)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = arvm.Email, Email = arvm.Email };
+                var result = await _userManager.CreateAsync(user, arvm.Password);
+
+                if (result.Succeeded)
+                {
+                    //Create a list where my claims will be added to
+                    List<Claim> myClaims = new List<Claim>();
+
+                    // claim for the User's role
+                    Claim claim1 = new Claim(ClaimTypes.Role, "Administrator", ClaimValueTypes.String);
+                    myClaims.Add(claim1);
+
+                    var addClaims = await _userManager.AddClaimsAsync(user, myClaims);
+
+                    if(addClaims.Succeeded)
+                    {
+                        await _signInManager.PasswordSignInAsync(arvm.Email, arvm.Password, true, lockoutOnFailure: false);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View("Forbidden");
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel lvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(lvm.Email, lvm.Password, lvm.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            string error = "Incorrect username or password";
+            ModelState.AddModelError("", error);
+            return View();
+        }
+
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [Authorize]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return View();
+        }
+    }
+}
