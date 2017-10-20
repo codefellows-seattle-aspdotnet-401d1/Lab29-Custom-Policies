9cf9f678df76643ca74bad10c81d0e63097d21d6
Gregory Dukes <kcils360@gmail.com>
2017-10-20 08:42:05 -0700

    created recipe model

---
+++ Lab29CustomPolicies/Lab29CustomPolicies/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab29CustomPolicies.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+
+        public string Dish { get; set; }
+
+        public string Category { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string Directions { get; set; }
+
+        public bool IsPublished { get; set; }
+    }
+}
